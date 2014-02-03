4400d6c53c73166847937053f1a09364617ba3a6
Tim Walker <tdskywalker@gmail.com>
2014-02-03 23:57:11 +0100

    WIP :-(

---
+++ libavformat/hevc.h
@@ -0,0 +1,34 @@
+/*
+ * HEVC helper functions for muxers
+ * Copyright (c) 2014 Tim Walker <tdskywalker@gmail.com>
+ *
+ * This file is part of Libav.
+ *
+ * Libav is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Libav is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Libav; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef AVFORMAT_HEVC_H
+#define AVFORMAT_HEVC_H
+
+#include <stdint.h>
+#include "avio.h"
+
+int ff_hevc_parse_nal_units(AVIOContext *s, const uint8_t *buf, int size);
+int ff_hevc_parse_nal_units_buf(const uint8_t *buf_in, uint8_t **buf, int *size);
+int ff_isom_write_hvcc(AVIOContext *pb, const uint8_t *data, int len);
+const uint8_t *ff_hevc_find_startcode(const uint8_t *p, const uint8_t *end);
+int ff_hevc_write_annexb_extradata(const uint8_t *in, uint8_t **buf, int *size);
+
+#endif /* AVFORMAT_HEVC_H */
