d3d366633e3958a8083b50b63014395484522038
Tim Walker <tdskywalker@gmail.com>
2014-02-21 20:10:38 +0100

    In progress.

--- libavformat/hevc.c
+++ libavformat/hevc.c
@@ -19,10 +19,10 @@
  */
 
 #include "libavcodec/get_bits.h"
+#include "libavcodec/golomb.h"
 #include "libavcodec/hevc.h"
 #include "libavutil/intreadwrite.h"
 #include "avc.h"
-#include "avformat.h"
 #include "avio.h"
 #include "hevc.h"
 
@@ -84,12 +84,118 @@ static void dump_profile_tier_level(const char *buf_name, uint8_t *buf, int inde
     av_log(NULL, AV_LOG_FATAL, "\n");
 }
 
+typedef struct HEVCDecoderConfigurationRecord {
+    //fixme
+} HEVCDecoderConfigurationRecord;
+
+static void parse_nal_header(GetBitContext *gb, uint8_t *nal_type)
+{
+    skip_bits1(gb); // forbidden_zero_bit
+
+    *nal_type = get_bits(gb, 6);
+
+    skip_bits(gb, 6); // nuh_layer_id
+    skip_bits(gb, 3); // nuh_temporal_id_plus1
+}
+
+static void decode_profile_tier_level(GetBitContext *gb, PTLCommon *ptl)
+{
+    int i;
+    
+    ptl->profile_space = get_bits (gb, 2);
+    ptl->tier_flag     = get_bits1(gb);
+    ptl->profile_idc   = get_bits (gb, 5);
+
+    for (i = 0; i < 32; i++)
+        ptl->profile_compatibility_flag[i] = get_bits1(gb);
+
+    ptl->progressive_source_flag    = get_bits1(gb);
+    ptl->interlaced_source_flag     = get_bits1(gb);
+    ptl->non_packed_constraint_flag = get_bits1(gb);
+    ptl->frame_only_constraint_flag = get_bits1(gb);
+    
+    skip_bits_long(gb, 32); // reserved_zero_44bits[ 0..31]
+    skip_bits     (gb, 12); // reserved_zero_44bits[32..43]
+}
+
+static void parse_ptl(GetBitContext *gb, PTL *ptl, int max_sub_layers_minus1)
+{
+    int i;
+
+    decode_profile_tier_level(gb, &ptl->general_ptl);
+    ptl->general_ptl.level_idc = get_bits(gb, 8);
+    
+    for (i = 0; i < max_sub_layers_minus1; i++) {
+        ptl->sub_layer_profile_present_flag[i] = get_bits1(gb);
+        ptl->sub_layer_level_present_flag[i]   = get_bits1(gb);
+    }
+
+    if (max_sub_layers_minus1 > 0)
+        for (i = max_sub_layers_minus1; i < 8; i++)
+            skip_bits(gb, 2); // reserved_zero_2bits[i]
+
+    for (i = 0; i < max_sub_layers_minus1; i++) {
+        if (ptl->sub_layer_profile_present_flag[i])
+            decode_profile_tier_level(gb, &ptl->sub_layer_ptl[i]);
+
+        if (ptl->sub_layer_level_present_flag[i])
+            ptl->sub_layer_ptl[i].level_idc = get_bits(gb, 8);
+    }
+}
+
+static int hevc_sps_to_hvcc(uint8_t *sps_buf, int sps_size,
+                            HEVCDecoderConfigurationRecord *hvcc)
+{
+    PTL ptl;
+    uint8_t nal_type;
+    GetBitContext gb;
+    int ret, sps_max_sub_layers_minus1;
+
+    ret = init_get_bits8(&gb, sps_buf, sps_size);
+    if (ret < 0)
+        return ret;
+
+    parse_nal_header(&gb, &nal_type);
+    if (nal_type != NAL_SPS)
+        return AVERROR_INVALIDDATA;
+
+    skip_bits(&gb, 4); // sps_video_parameter_set_id
+
+    sps_max_sub_layers_minus1 = get_bits(&gb, 3);
+
+    skip_bits1(&gb); // sps_temporal_id_nesting_flag
+
+    parse_ptl(&gb, &ptl, sps_max_sub_layers_minus1);
+
+    get_ue_golomb_long(&gb); // sps_seq_parameter_set_id
+
+    if (get_ue_golomb_long(&gb) == 3) // chroma_format_idc
+        skip_bits1(&gb); // separate_colour_plane_flag
+
+    get_ue_golomb_long(&gb); // pic_width_in_luma_samples
+    get_ue_golomb_long(&gb); // pic_height_in_luma_samples
+
+    if (get_bits1(&gb)) { // conformance_window_flag
+        get_ue_golomb_long(&gb); // conf_win_left_offset
+        get_ue_golomb_long(&gb); // conf_win_right_offset
+        get_ue_golomb_long(&gb); // conf_win_top_offset
+        get_ue_golomb_long(&gb); // conf_win_bottom_offset
+    }
+
+    get_ue_golomb_long(&gb); // bit_depth_luma_minus8
+    get_ue_golomb_long(&gb); // bit_depth_chroma_minus8
+    get_ue_golomb_long(&gb); // log2_max_pic_order_cnt_lsb_minus4
+
+    return 0;
+}
+
 int ff_isom_write_hvcc(AVIOContext *pb, const uint8_t *data, int len)
 {
     HEVC_DEBUG_LOG("data: %p, len: %d\n", data, len);
     if (len > 6) {
         /* check for H.265 start code */
         if (AV_RB32(data) == 0x00000001 || AV_RB24(data) == 0x000001) {
+            HEVCDecoderConfigurationRecord hvcc;
             uint32_t vps_size = 0, sps_size = 0, pps_size = 0;
             uint8_t *buf = NULL, *end, *start, *vps = NULL, *sps = NULL, *pps = NULL;
 
@@ -112,18 +218,7 @@ int ff_isom_write_hvcc(AVIOContext *pb, const uint8_t *data, int len)
                 if (ret < 0)
                     return ret;
 
-                // forbidden_zero_bit
-                if (get_bits1(&gb) != 0)
-                    return AVERROR_INVALIDDATA;
-
-                nal_type = get_bits(&gb, 6);
-
-                // nuh_layer_id
-                skip_bits(&gb, 6);
-
-                // nuh_temporal_id_plus1
-                if (!get_bits(&gb, 3))
-                    return AVERROR_INVALIDDATA;
+                parse_nal_header(&gb, &nal_type);
 
                 switch (nal_type) {
                 case NAL_VPS:
@@ -186,6 +281,10 @@ int ff_isom_write_hvcc(AVIOContext *pb, const uint8_t *data, int len)
             /* Log the contents of the vars where I think these values are */
             dump_profile_tier_level("VPS", vps, 9, -1);
             dump_profile_tier_level("SPS", sps, 6, -1);
+
+            ret = hevc_sps_to_hvcc(sps, sps_size, &hvcc);
+            if (ret < 0)
+                return ret;
 
             /* HEVCDecoderConfigurationRecord */
             avio_w8(pb,      1 ); /* configurationVersion */
