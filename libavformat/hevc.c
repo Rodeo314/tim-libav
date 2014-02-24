7d76d554094e7be59e0b35b6048ecfef0d42312a
Tim Walker <tdskywalker@gmail.com>
2014-02-24 01:20:39 +0100

    This should work too.

--- libavformat/hevc.c
+++ libavformat/hevc.c
@@ -28,6 +28,35 @@
 
 #define MAX_SPATIAL_SEGMENTATION 4096 // max. value of u(12) field
 
+typedef struct HEVCDecoderConfigurationRecord {
+    uint8_t  configurationVersion;
+    uint8_t  general_profile_space;
+    uint8_t  general_tier_flag;
+    uint8_t  general_profile_idc;
+    uint32_t general_profile_compatibility_flags;
+    uint64_t general_constraint_indicator_flags;
+    uint8_t  general_level_idc;
+    uint16_t min_spatial_segmentation_idc;
+    uint8_t  parallelismType;
+    uint8_t  chromaFormat;
+    uint8_t  bitDepthLumaMinus8;
+    uint8_t  bitDepthChromaMinus8;
+    uint16_t avgFrameRate;
+    uint8_t  constantFrameRate;
+    uint8_t  numTemporalLayers;
+    uint8_t  temporalIdNested;
+    uint8_t  lengthSizeMinusOne;
+} HEVCDecoderConfigurationRecord;
+
+typedef struct HVCCProfileTierLevel {
+    uint8_t  profile_space;
+    uint8_t  tier_flag;
+    uint8_t  profile_idc;
+    uint32_t profile_compatibility_flags;
+    uint64_t constraint_indicator_flags;
+    uint8_t  level_idc;
+} HVCCProfileTierLevel;
+
 static void nal_unit_parse_header(GetBitContext *gb, uint8_t *nal_type)
 {
     skip_bits1(gb); // forbidden_zero_bit
--- libavformat/hevc.h
+++ libavformat/hevc.h
@@ -35,35 +35,6 @@ static av_unused int binar_ize(uint8_t n)
     return ret;
 }
 
-typedef struct HEVCDecoderConfigurationRecord {
-    uint8_t  configurationVersion;
-    uint8_t  general_profile_space;
-    uint8_t  general_tier_flag;
-    uint8_t  general_profile_idc;
-    uint32_t general_profile_compatibility_flags;
-    uint64_t general_constraint_indicator_flags;
-    uint8_t  general_level_idc;
-    uint16_t min_spatial_segmentation_idc;
-    uint8_t  parallelismType;
-    uint8_t  chromaFormat;
-    uint8_t  bitDepthLumaMinus8;
-    uint8_t  bitDepthChromaMinus8;
-    uint16_t avgFrameRate;
-    uint8_t  constantFrameRate;
-    uint8_t  numTemporalLayers;
-    uint8_t  temporalIdNested;
-    uint8_t  lengthSizeMinusOne;
-} HEVCDecoderConfigurationRecord;
-
-typedef struct HVCCProfileTierLevel {
-    uint8_t  profile_space;
-    uint8_t  tier_flag;
-    uint8_t  profile_idc;
-    uint32_t profile_compatibility_flags;
-    uint64_t constraint_indicator_flags;
-    uint8_t  level_idc;
-} HVCCProfileTierLevel;
-
 int ff_isom_write_hvcc(AVIOContext *pb, const uint8_t *data, int len);
 
 #endif /* AVFORMAT_HEVC_H */
